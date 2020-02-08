78b8db4c88f3e76a46d5ec63e6003b3079c5ac54
bebopvinh <vinhdo07@gmail.com>
2020-02-07 22:23:02 -0800

    add explanation for url, remove console.log

--- src/components/jokes/jokeButton.js
+++ src/components/jokes/jokeButton.js
@@ -4,12 +4,12 @@ function JokeButton(props) {
   const { setJoke } = props
   const handleClick = async event => {
     event.preventDefault()
+    //RESTful route has blacklistFlags query because some jokes are too dark...
     const url =
       "https://sv443.net/jokeapi/v2/joke/Dark?blacklistFlags=nsfw,racist,sexist&type=twopart"
     let response = await fetch(url)
     const data = await response.json()
     await setJoke(data)
-    await console.log(data)
   }
 
   return <button onClick={handleClick}>Humor Me!</button>

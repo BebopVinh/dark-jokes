d1ae9e2074076634acbafa83b4f62150a2af821a
bebopvinh <vinhdo07@gmail.com>
2020-02-07 22:22:21 -0800

    add HTML for card-style component, classes for CSS

--- src/components/jokes/card.js
+++ src/components/jokes/card.js
@@ -1,16 +1,30 @@
 import React from "react"
+import "./card.css"
 
-function Card(props) {
+const Card = props => {
   const { joke } = props
-  console.log()
-  const renderJokes = joke => {
-    debugger
+  const renderJokes = () => {
+    if (joke) {
+      console.log("joke is ", joke)
+      return (
+        <div className="container">
+          <div className="card-container">
+            <div className="card">
+              <figure className="front">
+                <p>{joke.setup}</p>
+              </figure>
+              <figure className="back">
+                <p>{joke.delivery}</p>
+              </figure>
+            </div>
+          </div>
+        </div>
+      )
+    } else {
+      return <p>Not funny yet...</p>
+    }
   }
-  return (
-    <div>
-      <p>{renderJokes()}</p>
-    </div>
-  )
+  return <div>{renderJokes()}</div>
 }
 
 export default Card
